9d7d3d89441a2a288db2a85393b879263123471a
Ryan Dahl <ry@tinyclouds.org>
2018-07-12 19:06:49 -0400

    Add deno_third_party as git submodule.

--- tools/third_party_commit.py
+++ tools/third_party_commit.py
@@ -11,6 +11,7 @@ import os
 from os.path import join
 import subprocess
 from util import run, remove_and_symlink
+import shutil
 
 root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 third_party_path = join(root_path, "third_party")
@@ -20,14 +21,17 @@ try:
 except:
     pass
 os.chdir(third_party_path)
-remove_and_symlink(join("..", "gclient_config.py"), ".gclient")
-remove_and_symlink(join("..", "package.json"), "package.json")
-remove_and_symlink(join("..", "yarn.lock"), "yarn.lock")
-remove_and_symlink(join("v8", "third_party", "googletest"), "googletest")
-remove_and_symlink(join("v8", "third_party", "jinja2"), "jinja2")
-remove_and_symlink(join("v8", "third_party", "llvm-build"), "llvm-build")
-remove_and_symlink(join("v8", "third_party", "markupsafe"), "markupsafe")
-run(["gclient", "sync", "-j8", "--shallow", "--no-history", "--nohooks"])
-#run(["gclient", "sync", "--shallow", "--no-history", "--nohooks"])
-#run(["gclient", "runhooks"])
-run(["yarn"])
+gitdirs = subprocess.check_output("find . | egrep .git$", shell=True).strip().split("\n")
+for gd in gitdirs:
+    if gd == "./.git" or gd == ".git":
+        continue
+    repo_git_dir = os.path.join(third_party_path, gd)
+    repo = os.path.dirname(repo_git_dir)
+    os.chdir(repo)
+    sha = subprocess.check_output("git rev-list HEAD -1", shell=True).strip()
+    # TODO Sanity check sha against those stored in gclient_config.py.
+    print sha, repo
+    print "rm -rf " + repo_git_dir
+    shutil.rmtree(repo_git_dir) # Delete the .git directories.
+
+os.chdir(third_party_path)
